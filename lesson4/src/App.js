bb4232d228e7e466cae00f11112b3e2fa9a591fe
benyueer <347201906@qq.com>
2022-03-27 20:33:45 +0800

    feat: router 01

---
+++ lesson4/src/App.js
@@ -0,0 +1,35 @@
+/* eslint-disable react/jsx-pascal-case */
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom'
+import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+import _404 from './pages/_404'
+import UserPage from './pages/UserPage'
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Link to="/">首页</Link>
+        <Link to="/user">用户</Link>
+        <Link to="/login">登录</Link>
+        <Link to="/product/123">商品</Link>
+
+        <Switch>
+          <Route 
+            exact 
+            path="/" 
+            children={() => <div>ccc</div>}
+            render={() => <div>rander</div>}
+            component={HomePage} 
+          />
+          <Route path="/user" component={UserPage} />
+          <Route path="/login" component={LoginPage} />
+          <Route component={_404} />
+        </Switch>
+
+      </Router>
+    </div>
+  )
+}
+
+export default App
---
+++ lesson4/src/pages/_404.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class _404 extends Component {
+  render() {
+    return (
+      <div>_404</div>
+    )
+  }
+}

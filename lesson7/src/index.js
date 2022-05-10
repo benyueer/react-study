298732a0147326f0886028d698e85ae5a398435d
wuji <shidongzhang@xyz10.com>
2022-05-10 16:35:39 +0800

    feat: lesson7

---
+++ lesson7/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const jsx = (
+  <div>
+    <span>hello</span>
+  </div>
+)
+ReactDOM.render(jsx, document.getElementById('root'));

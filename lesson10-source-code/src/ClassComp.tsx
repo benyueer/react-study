f3893e0680f2d8b0415b65f13c50e35487197a06
benyueer <347201906@qq.com>
2023-06-14 17:16:54 +0800

    feat: react study

--- lesson10-source-code/src/ClassComp.tsx
+++ lesson10-source-code/src/ClassComp.tsx
@@ -1,18 +1,43 @@
 import React, { Component } from 'react'
 
-export default class ClassComp extends Component {
+class FCmp extends Component {
+  render() {
+    return (
+      <div>123</div>
+    )
+  }
+}
+
+export default class ClassComp extends FCmp {
   state = {
     count: 0
   }
+
+  componentDidMount(): void {
+
+      console.log(super.render())
+
+      document.querySelector('#count')?.addEventListener('click', () => {
+        debugger
+        this.setState({
+          count: this.state.count + 1
+        })
+        console.log(this.state)
+      })
+  }
+
   render() {
     return (
       <div>
         <p>ClassComp</p>
         <span onClick={() => {
+          debugger
           this.setState({
             count: this.state.count + 1
           })
+          console.log(this.state)
         }}>{this.state.count}</span>
+        <span id="count">{this.state.count}</span>
       </div>
     )
   }
